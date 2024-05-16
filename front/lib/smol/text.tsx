7daa9a979697fd6af839a2e6cb37b8b4e2b7225b
Jayden Richardson <jayrich.dev@gmail.com>
2024-05-16 19:34:10 +0100

    test

--- front/lib/smol/text.tsx
+++ front/lib/smol/text.tsx
@@ -183,24 +183,6 @@ export class ThreeCanvas {
     this.renderLoop()
   }
 
-  private orbitControls(): void {
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-    this.controls.enableDamping = true
-    this.controls.enabled = false
-    this.controls.enablePan = false
-    this.controls.enableRotate = false
-    this.controls.enableZoom = false
-
-    // Add parallax effect
-    document.addEventListener('mousemove', (event) => {
-      const parallaxX = (event.clientX / window.innerWidth) * 2 - 1
-      const parallaxY = (event.clientY / window.innerHeight) * 2 - 1
-
-      this.camera.position.x += (parallaxX - this.camera.position.x) * 0.05
-      this.camera.position.y += (-parallaxY - this.camera.position.y) * 0.05
-      this.camera.lookAt(this.scene.position)
-    })
-  }
 
   private createStats(): void {
     this.stats.showPanel(0)
@@ -209,29 +191,49 @@ export class ThreeCanvas {
     document.body.appendChild(this.stats.dom)
   }
 
-  private setupCamera(): void {
-    const aspect = window.innerWidth / window.innerHeight
-    const frustumSize = this.config.view_size
-    const frustumHalfHeight = frustumSize / 2
-    const frustumHalfWidth = frustumHalfHeight * aspect
-
-    this.camera = new THREE.OrthographicCamera(
-      -frustumHalfWidth,
-      frustumHalfWidth,
-      frustumHalfHeight,
-      -frustumHalfHeight,
-      -100,
-      1000
-    )
-    this.camera.position.set(this.config.camera_pos_x, this.config.camera_pos_y + 7, this.config.camera_pos_z) // Raise camera slightly on y-axis
-
-    // Ensuring the camera looks at the center of the scene
-    this.camera.lookAt(new THREE.Vector3(this.scene.position.x, this.scene.position.y + 7, this.scene.position.z))
-
-    // Update projection matrix during initialization and on window resize
-    this.updateProjectionMatrix()
-    window.addEventListener('resize', this.updateProjectionMatrix.bind(this))
-  }
+	private setupCamera(): void {
+		const aspect = window.innerWidth / window.innerHeight;
+		const frustumSize = this.config.view_size;
+		const frustumHalfHeight = frustumSize / 2;
+		const frustumHalfWidth = frustumHalfHeight * aspect;
+	
+		this.camera = new THREE.OrthographicCamera(
+			-frustumHalfWidth,
+			frustumHalfWidth,
+			frustumHalfHeight,
+			-frustumHalfHeight,
+			-100,
+			1000
+		);
+	
+		this.camera.position.set(this.config.camera_pos_x, this.config.camera_pos_y + 5.5, this.config.camera_pos_z); // Raise camera slightly on y-axis
+	
+		// Ensuring the camera looks at the center of the scene
+		this.camera.lookAt(this.scene.position);
+	
+		// Update projection matrix during initialization and on window resize
+		this.updateProjectionMatrix();
+		window.addEventListener('resize', this.updateProjectionMatrix.bind(this));
+	}
+	
+	private orbitControls(): void {
+		this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+		this.controls.enableDamping = true;
+		this.controls.enabled = false; // Disable OrbitControls
+		this.controls.enablePan = false;
+		this.controls.enableRotate = false;
+		this.controls.enableZoom = false;
+	
+		// Add parallax effect
+		document.addEventListener('mousemove', (event) => {
+			const parallaxX = (event.clientX / window.innerWidth) * 2 - 1;
+			const parallaxY = (event.clientY / window.innerHeight) * 2 - 1;
+	
+			this.camera.position.x += (parallaxX - this.camera.position.x) * 0.05;
+			this.camera.position.y += (-parallaxY - this.camera.position.y) * 0.05;
+			this.camera.lookAt(this.scene.position);
+		});
+	}
 
   private updateProjectionMatrix(): void {
     const aspect = window.innerWidth / window.innerHeight
